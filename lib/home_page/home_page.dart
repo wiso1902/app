65016459ad472b96965b9d1e43df61e0738c327e
william sonesson <wiso1902@student.miun.se>
2024-02-17 18:08:57 +0100

    1

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import '../auth_service.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoButton Sample'),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            CupertinoButton.filled(
+              onPressed: () {context.read<AuthenticationService>().signOut();},
+              child: const Text('Enabled'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
