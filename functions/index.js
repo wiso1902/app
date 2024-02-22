1c8610b82cc9f7781f718422677aa4ecff4a068c
william sonesson <wiso1902@student.miun.se>
2024-02-22 14:56:52 +0100

    1.3 - firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.disableNewAccounts = functions.auth.user().onCreate((user) => {
+  return admin.auth().updateUser(user.uid, {
+    disabled: true,
+  });
+});
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
